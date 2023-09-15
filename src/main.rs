70b8dacf93f0461bf72f3c5c49a1bf68e3eb9552
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-09-15 15:57:28 +0800

    login reqest

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,55 @@
 #![allow(unused)] // For beginning only.
 
+pub use self::error::{Error, Result};
+
 use std::net::SocketAddr;
 
 use axum::{
     extract::{Path, Query},
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, get_service},
     Router,
 };
 use serde::Deserialize;
+use tower_http::services::ServeDir;
+
+mod error;
+mod web;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route(
-            "/hello",
-            get(handler_hello), // get(|| async { Html("Hello <strong>World!!!</strong>") }),
-        )
-        .route("/hello2/:name", get(handler_hello2));
+    // let routes_hello = Router::new()
+    //     .route(
+    //         "/hello",
+    //         get(handler_hello), // get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    //     )
+    //     .route("/hello2/:name", get(handler_hello2));
+
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .fallback_service(routes_static());
 
     // region:  --- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        // .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
     // endregion:  --- Start Server
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+// region: --- Routes Hello
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
 }
 
 // region: --- Handler Hello
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,6 +1,7 @@
 #![allow(unused)] // For beginning only.
 
 use anyhow::Result;
+use serde_json::json;
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
@@ -8,6 +9,16 @@ async fn quick_dev() -> Result<()> {
 
     hc.do_get("/hello?name=Qiao").await?.print().await?;
     hc.do_get("/hello2/Mike").await?.print().await?;
+    // hc.do_get("/src/main.rs").await?.print().await?;
+
+    let req_login = hc.do_post(
+        "/api/login",
+        json!({
+            "username": "demo11",
+            "pwd": "welcome"
+        }),
+    );
+    let resp_login = req_login.await?.print().await?;
 
     Ok(())
 }
