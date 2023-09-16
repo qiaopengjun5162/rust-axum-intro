6ebcca5ae15acd8fa1407dffa8412ee1456c98df
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-09-16 12:48:00 +0800

    ctx cookie update

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,7 @@
 //! Simplistic Model Layer
 //! (with mock-store layer)
 
-use crate::{Error, Result};
+use crate::{ctx::Ctx, Error, Result};
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 
@@ -9,6 +9,7 @@ use std::sync::{Arc, Mutex};
 #[derive(Debug, Serialize, Clone)]
 pub struct Ticket {
     pub id: u64,
+    pub cid: u64, // creator user_id
     pub title: String,
 }
 
@@ -34,19 +35,20 @@ impl ModelController {
 
 // CRUD Implementation
 impl ModelController {
-    pub async fn create_ticket(&self, ticket_fc: TicketForCreate) -> Result<Ticket> {
+    pub async fn create_ticket(&self, ctx: Ctx, ticket_fc: TicketForCreate) -> Result<Ticket> {
         /// 创建票据
         let mut store = self.tickets_store.lock().unwrap();
         let id = store.len() as u64;
         let ticket = Ticket {
             id,
+            cid: ctx.user_id(),
             title: ticket_fc.title,
         };
         store.push(Some(ticket.clone()));
         Ok(ticket)
     }
 
-    pub async fn list_tickets(&self) -> Result<Vec<Ticket>> {
+    pub async fn list_tickets(&self, _ctx: Ctx) -> Result<Vec<Ticket>> {
         // 创建了一个store变量，它是一个互斥锁，用于保护门票信息的存储。在这里使用了lock()方法获取互斥锁，并使用unwrap()方法将其转换为Result类型。
         let store = self.tickets_store.lock().unwrap();
         // 使用了store.iter()方法遍历门票信息，并使用filter_map()方法过滤掉无效的门票信息。最后使用collect()方法将有效的门票信息收集到一个Vec中。
@@ -54,7 +56,7 @@ impl ModelController {
         Ok(tickets)
     }
 
-    pub async fn delete_ticket(&self, id: u64) -> Result<Ticket> {
+    pub async fn delete_ticket(&self, _ctx: Ctx, id: u64) -> Result<Ticket> {
         // 这段代码的作用是删除一个票据。它首先获取了一个可变的引用store，然后使用id作为索引从store中获取票据并将其移除。如果成功获取到票据，则返回该票据；否则返回一个包含错误信息的Result。
         let mut store = self.tickets_store.lock().unwrap();
         // 使用id作为索引从store中获取票据，并使用and_then方法对获取的票据进行操作。在这里，使用了一个闭包(|t| t.take())来获取票据并将其从store中移除。
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -29,7 +29,7 @@ async fn quick_dev() -> Result<()> {
     );
     req_create_ticket.await?.print().await?;
 
-    hc.do_delete("/api/tickets/1").await?.print().await?;
+    // hc.do_delete("/api/tickets/1").await?.print().await?;
 
     hc.do_get("/api/tickets").await?.print().await?;
 
