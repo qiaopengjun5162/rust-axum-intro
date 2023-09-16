17265c807b0bc5e5efb18ec7a84b96452a2dfc73
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-09-16 15:12:29 +0800

    ctx error update response

--- src/error.rs
+++ src/error.rs
@@ -5,13 +5,14 @@ use axum::{
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Clone, Debug, strum_macros::AsRefStr)]
 pub enum Error {
     LoginFail,
 
     // -- Auth errors.
     AuthFailNoAuthTokenCookie,
     AuthFailTokenWrongFormat,
+    AuthFailCtxNotInRequestExt,
 
     // -- Model errors.
     TicketDeleteFailIdNotFound { id: u64 },
@@ -22,6 +23,51 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+
+        // Create a placeholder Axum response.
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the response.
+        response.extensions_mut().insert(self);
+
+        response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        // #[allow(unreachable_patterns)] 是一个编译器指令，用于允许在 match 语句中存在无法到达的模式。
+        // 通常，match 语句应该覆盖所有可能的模式，但有时可能会有无法到达的模式。
+        // 使用 #[allow(unreachable_patterns)] 可以告诉编译器忽略这些无法到达的模式而不产生警告。
+        #[allow(unreachable_patterns)]
+        match self {
+            //    -- Login.
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            //    -- Auth.
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            //    -- Model.
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            // -- Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)] // 允许将枚举成员名称作为字符串引用返回
+#[allow(non_camel_case_types)] // 用于允许使用非驼峰命名的类型名称
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,14 +1,15 @@
 use async_trait::async_trait;
-use axum::extract::FromRequestParts;
+use axum::extract::{FromRequestParts, State};
 use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
 use axum::RequestPartsExt;
 use lazy_regex::regex_captures;
-use tower_cookies::Cookies;
+use tower_cookies::{Cookie, Cookies};
 
 use crate::ctx::Ctx;
+use crate::model::ModelController;
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
@@ -34,6 +35,39 @@ pub async fn mw_require_auth<B>(
     Ok(next.run(req).await)
 }
 
+pub async fn mw_ctx_resolver<B>(
+    _mc: State<ModelController>,
+    cookies: Cookies,
+    mut req: Request<B>,
+    next: Next<B>,
+) -> Result<Response> {
+    println!("->> {:<12} - mw_ctx_resolver", "MIDDLEWARE");
+
+    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+    // Compute Result<Ctx>.
+    let result_ctx = match auth_token
+        .ok_or(Error::AuthFailNoAuthTokenCookie)
+        .and_then(parse_token)
+    {
+        Ok((user_id, _exp, _sign)) => {
+            // TODO: Token components validation.
+            Ok(Ctx::new(user_id))
+        }
+        Err(e) => Err(e),
+    };
+
+    // Remove the cookie if something went wrong other than NoAuthTokenCookie.
+    if result_ctx.is_err() && !matches!(result_ctx, Err(Error::AuthFailNoAuthTokenCookie)) {
+        cookies.remove(Cookie::named(AUTH_TOKEN));
+    }
+
+    // Store the ctx_result in the request extension.
+    req.extensions_mut().insert(result_ctx);
+
+    Ok(next.run(req).await)
+}
+
 // region: --- Ctx Extractor
 #[async_trait]
 impl<S: Send + Sync> FromRequestParts<S> for Ctx {
@@ -42,19 +76,25 @@ impl<S: Send + Sync> FromRequestParts<S> for Ctx {
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
         println!("->> {:<12} - Ctx", "EXTRACTOR");
 
-        // User the cookies extractor.
-        let cookies = parts.extract::<Cookies>().await.unwrap();
+        // // User the cookies extractor.
+        // let cookies = parts.extract::<Cookies>().await.unwrap();
 
-        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+        // let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
 
-        // Parse token.
-        let (user_id, exp, sign) = auth_token
-            .ok_or(Error::AuthFailNoAuthTokenCookie)
-            .and_then(parse_token)?;
+        // // Parse token.
+        // let (user_id, exp, sign) = auth_token
+        //     .ok_or(Error::AuthFailNoAuthTokenCookie)
+        //     .and_then(parse_token)?;
 
-        // TODO: Token components validation
+        // // TODO: Token components validation
 
-        Ok(Ctx::new(user_id))
+        // Ok(Ctx::new(user_id))
+
+        parts
+            .extensions
+            .get::<Result<Ctx>>()
+            .ok_or(Error::AuthFailCtxNotInRequestExt)?
+            .clone()
     }
 }
 // endregion: --- Ctx Extractor
